16e6f0d11cc5d1b02a19226e31f313c39180f95e
zh0ng <livividcs@gmail.com>
2016-12-10 18:29:58 +0800

    新增按钮事件，可以移动和缩放

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gl/glut.h"
+
+GLfloat originX = 0.0;
+GLfloat originY = 0.0;
+GLfloat originZ = -20.0;
+
 // 行星
 GLfloat rot0 = 0.0;
 GLfloat rot1 = 0.0;
@@ -15,19 +20,11 @@ GLfloat rot8 = 0.0;
 GLfloat rot9 = 0.0;
 
 // 轨道宽度
-GLfloat INNER_TORUS_RADIUS = 0.02;
+const GLfloat INNER_TORUS_RADIUS = 0.02;
 
 void solidSphere(GLdouble radius, GLint slices = 32, GLint stacks = 32)
 {
 	glutSolidSphere(radius, slices, stacks);
-}
-
-void init()
-{
-	// 设置背景色为黑色
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glClearDepth(1.0);
-	glShadeModel(GL_FLAT);
 }
 
 void drawPlanet(GLfloat r, GLfloat g, GLfloat b, GLfloat torus_radius, GLfloat angularSpeed, 
@@ -35,13 +32,13 @@ void drawPlanet(GLfloat r, GLfloat g, GLfloat b, GLfloat torus_radius, GLfloat a
 {
 	glPushMatrix();
 	glColor3f(r, g, b);
-	// 绘制辅助轨道
+	// 辅助轨道
 	glRotatef(90, 1.0, 0, 0.0);
 	glutSolidTorus(INNER_TORUS_RADIUS, torus_radius, 10, 64);
 	glRotatef(-90, 1.0, 0, 0.0);
-	// 设置公转速度
+	// 公转速度
 	glRotatef(angularSpeed, 0.0, 1.0, 0.0);
-	// 设置公转半径
+	// 公转半径
 	glTranslatef(torus_radius, 0.0, 0.0);
 	solidSphere(radius);
 	if (fun != NULL)
@@ -52,12 +49,10 @@ void drawPlanet(GLfloat r, GLfloat g, GLfloat b, GLfloat torus_radius, GLfloat a
 // 绘制地球的卫星-月亮
 void drawMoon()
 {
-	glColor3f(0.5, 0.6, 0.5);
-	// 设置月亮公转速度
+	const GLfloat r = 0.6;
+	glColor3f(0.5, r, 0.5);
 	glRotatef(rot9, 0.0, 1.0, 0.0);
-	// 设置月亮公转半径
-	glTranslatef(0.6, 0.0, 0.0);
-	// 绘制月亮
+	glTranslatef(r, 0.0, 0.0);
 	solidSphere(0.1, 10, 8);
 }
 
@@ -75,65 +70,55 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
 	glLoadIdentity();
-	//gluLookAt(0, 10, 10, 0, 0, 0, 0, 1, 0);
-	//glRotatef(45.0, 0.0, 0.0, 1.0);
-	glTranslatef(0.0, 0.0, -20.0);
+
+	glTranslatef(originX, originY, originZ);
 	glRotatef(90.0, 1.0, 0.0, 0);
-	// 绘制太阳
+	// 太阳
 	glColor3f(1.0, 0.0, 0.0);
 	solidSphere(2.0);
 	
-	// 绘制地球
+	// 地球
 	drawPlanet(0.0, 0.0, 1.0, 5.0, rot0, 0.4, drawMoon);
-
-	// 绘制水星
+	// 水星
 	drawPlanet(0.0, 1.0, 1.0, 2.5, rot1, 0.2);
-
-	// 绘制金星
+	// 金星
 	drawPlanet(0.0, 1.0, 0.0, 3.4, rot2, 0.3);
-
-	// 绘制火星
+	// 火星
 	drawPlanet(1.0, 0.0, 0.0, 6.6, rot3, 0.5);
-
-	// 绘制木星
+	// 木星
 	drawPlanet(1.0, 0.1, 1.0, 8.5, rot4, 1.0);
-
-	// 绘制土星
+	// 土星
 	drawPlanet(1.0, 1.0, 0.0, 12.5, rot5, 0.85, drawSaturnRing);
-
-	// 绘制天王星
+	// 天王星
 	drawPlanet(0.0, 1.0, 1.0, 15.5, rot6, 0.15);
-
-	// 绘制海王星
+	// 海王星
 	drawPlanet(0.0, 0.0, 0.5, 17.5, rot7, 0.145);
-
-	// 绘制冥王星
+	// 冥王星
 	drawPlanet(0.5, 0.5, 0.5, 19.5, rot8, 0.145);
 
 	glutSwapBuffers();
-
 	glFlush();
 }
 
-void rotate(GLfloat* rot, GLfloat angle)
+void rotate(GLfloat& rot, GLfloat angle)
 {
-	*rot += angle;
-	if (*rot >= 360.0)
-		*rot -= 360.0;
+	rot += angle;
+	if (rot >= 360.0)
+		rot -= 360.0;
 }
 
 void idle()
 {
-	rotate(&rot0, 0.1);
-	rotate(&rot1, 0.416);
-	rotate(&rot2, 0.1631);
-	rotate(&rot3, 0.053);
-	rotate(&rot4, 0.0083);
-	rotate(&rot5, 0.0034);
-	rotate(&rot6, 0.00119);
-	rotate(&rot7, 0.00069);
-	rotate(&rot8, 0.0008);
-	rotate(&rot9, 1.0);
+	rotate(rot0, 0.1);
+	rotate(rot1, 0.416);
+	rotate(rot2, 0.1631);
+	rotate(rot3, 0.053);
+	rotate(rot4, 0.0083);
+	rotate(rot5, 0.0034);
+	rotate(rot6, 0.00119);
+	rotate(rot7, 0.00069);
+	rotate(rot8, 0.0008);
+	rotate(rot9, 1.0);
 
 	glutPostRedisplay();
 }
@@ -143,8 +128,40 @@ void reshape(int w, int h)
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0, (GLdouble)w / (GLdouble)h, 1.0, 100.0);
+	gluPerspective(70.0, (GLdouble)w / (GLdouble)h, 1.0, 100.0);
 	glMatrixMode(GL_MODELVIEW);
+}
+
+void onSpecialKeyDown(int key, int x, int y)
+{
+	if (key == 101)
+	{
+		originY -= 0.2;
+	}
+	else if (key == 103)
+	{
+		originY += 0.2;
+	}
+	else if (key == 100)
+	{
+		originX += 0.2;
+	}
+	else if (key == 102)
+	{
+		originX -= 0.2;
+	}
+}
+
+void onNormalKeyDown(unsigned char key, int x, int y)
+{
+	if (key == '-')
+	{
+		originZ -= 0.2;
+	}
+	else if (key == '=')
+	{
+		originZ += 0.2;
+	}
 }
 
 void main(int argc, char **argv)
@@ -152,10 +169,17 @@ void main(int argc, char **argv)
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(800, 450);
-	glutInitWindowPosition(500, 500);
+	glutInitWindowSize(900, 600);
+	glutInitWindowPosition(80, 80);
 	glutCreateWindow("九大行星运行模拟");
-	init();
+	
+	glutKeyboardFunc(onNormalKeyDown);
+	glutSpecialFunc(onSpecialKeyDown);
+
+	// 设置背景色为黑色
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClearDepth(1.0);
+	glShadeModel(GL_FLAT);
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
